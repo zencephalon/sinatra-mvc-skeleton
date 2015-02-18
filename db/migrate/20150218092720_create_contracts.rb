194d96ef383174d55d19f75f5af036351610fb62
gabalicious <gabalicious113@gmail.com>
2015-02-18 18:41:45 -0500

    Add Name attribute for Contracts

--- db/migrate/20150218092720_create_contracts.rb
+++ db/migrate/20150218092720_create_contracts.rb
@@ -1,6 +1,7 @@
 class CreateContracts < ActiveRecord::Migration
 	def change
 		create_table :contracts do |t|
+			t.string :name
 			t.string :status
 			t.integer :maker_id, :references => "Company"
 			t.integer :taker_id, :references => "Company"
