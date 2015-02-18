cc09b4e92105115ada9cf36b130b4a25fdfcd0dd
gabalicious <gabalicious113@gmail.com>
2015-02-18 11:59:48 -0500

    Fix associationg with comment and log

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,5 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
-	belongs_to :log
 	belongs_to :contract
 	belongs_to :company
 	end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,6 +1,6 @@
 class Log < ActiveRecord::Base
-   has_one :user
-   has_one :maker, :class_name => 'Company'
-   has_one :taker, :class_name => 'Company'
-   has_one :comment
+   belongs_to :user
+   belongs_to :company
+   belongs_to :comment
+   belongs_to :contract
 end
