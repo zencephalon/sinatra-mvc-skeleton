f0cd60f1e4269b1759cf0a263553af305711d11e
gabalicious <gabalicious113@gmail.com>
2015-02-18 10:19:54 -0500

    Add Log Migration

---
+++ db/migrate/20150218092800_create_logs.rb
@@ -0,0 +1,10 @@
+class CreateLogs < ActiveRecord::Migration
+  def change
+  	   create_table :logs do |t|
+      t.string :action
+      t.references :company
+      t.references :contract
+      t.references :user
+      t.references :comment
+  end
+end
