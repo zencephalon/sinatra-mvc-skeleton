f58f18494f70ff394fe76a190fff75c2934f8f71
gabalicious <gabalicious113@gmail.com>
2015-02-18 10:18:16 -0500

    Modify User Migration

--- db/migrate/20141020120711_create_users.rb
+++ db/migrate/20141020120711_create_users.rb
@@ -1,8 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.string :password_digest
-    end
-  end
+	def change
+		create_table :users do |t|
+			t.string :username
+			t.string :password_digest
+			t.string :email
+			t.string :first_name
+			t.string :last_name
+			t.references :company
+			t.references :comment 
+		end
+	end
 end
