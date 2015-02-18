33fe2aeac1686e29ef9acc84c1aa03c202410e67
gabalicious <gabalicious113@gmail.com>
2015-02-18 10:20:07 -0500

    Add Comment Migration

---
+++ db/migrate/20150218092814_create_comments.rb
@@ -0,0 +1,9 @@
+class CreateComments < ActiveRecord::Migration
+	def change
+		create_table :comments do |t|
+			t.string :comment
+			t.text :email
+			t.references :user
+			t.references :company
+		end
+	end
