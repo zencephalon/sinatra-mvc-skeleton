b7baf56a517eccd1721d03e43d771a04be2e5066
gabalicious <gabalicious113@gmail.com>
2015-02-18 12:01:24 -0500

    Fix contracts and comments relations

--- db/migrate/20150218092720_create_contracts.rb
+++ db/migrate/20150218092720_create_contracts.rb
@@ -2,8 +2,8 @@ class CreateContracts < ActiveRecord::Migration
 	def change
 		create_table :contracts do |t|
 			t.string :status
-			t.integer :maker, :references => "Company"
-			t.integer :taker, :references => "Company"
+			t.integer :maker_id, :references => "Company"
+			t.integer :taker_id, :references => "Company"
 			t.text :maker_agreement
 			t.text :taker_agreement
 		end
--- db/migrate/20150218092814_create_comments.rb
+++ db/migrate/20150218092814_create_comments.rb
@@ -1,10 +1,11 @@
 class CreateComments < ActiveRecord::Migration
 	def change
 		create_table :comments do |t|
-			t.string :comment
-			t.text :email
+			t.string :content
+			t.string :ref_type
 			t.references :user
 			t.references :company
+			t.references :contract
 		end
 	end
 end
