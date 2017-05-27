f029e19a18de385e46060b526ab02660b5e166cd
Iliyan Ganev <ganeviliyan@gmail.com>
2017-05-26 17:49:26 -0700

    put key

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,3 +1,19 @@
 class Forecast < ActiveRecord::Base
   belongs_to :user
+
+
+  def response(city)
+
+    response = HTTParty.get("http://api.wunderground.com/api/putapikey/conditions/q/CA/#{city}.json")
+
+
+    weather = response['current_observation']['weather']
+    temp = response['current_observation']['temp_c']
+
+    p weather
+    p "temp is #{temp} C"
+
+  end
+
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -15,6 +15,7 @@ require 'pg'
 require 'active_record'
 require 'logger'
 require 'bcrypt'
+require 'httparty'
 
 require 'sinatra'
 require "sinatra/reloader" if development?
