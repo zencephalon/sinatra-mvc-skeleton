ba9f43e518cdf2abbb6e1f68f25f4774be6f0418
Iliyan Ganev <ganeviliyan@gmail.com>
2017-07-19 18:26:45 -0700

    Add forecast queries for next 5 days

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -18,11 +18,11 @@ post '/forecasts' do
   if @forecast.save
     country = (@forecast.country).split.map(&:capitalize).join('_')
     city = (@forecast.city).split.map(&:capitalize).join('_')
-
     @city = (@forecast.city).split.map(&:capitalize).join(' ')
     @country = (@forecast.country).split.map(&:capitalize).join(' ')
 
-    p weather_json = @forecast.response_weather(country, city)
+    weather_json = @forecast.response_weather(country, city)
+    weather10day_jason = @forecast.forecast_10_day(country, city)
 
     @weather = weather_json['current_observation']['weather']
     @icon = weather_json['current_observation']['icon_url']
@@ -31,9 +31,43 @@ post '/forecasts' do
     @wind = weather_json['current_observation']['wind_kph']
     @wind_miles = weather_json['current_observation']['wind_mph']
     @humidity = weather_json['current_observation']['relative_humidity']
-    
- 
-    # @local_weather = @forecast.local_weather
+
+    @forecast_next_weekday = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['date']['weekday'] 
+    @forecast_next_weekday_day = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['date']['day']   
+    @forecast_next_weekday_month = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['date']['month']   
+    @forecast_next_weekday_year = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['date']['year']   
+    @forecast_next_weekday_temp_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['high']['celsius'] 
+    @forecast_next_weekday_temp_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['high']['fahrenheit']
+    @forecast_next_weekday_temp_low_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['low']['celsius']
+    @forecast_next_weekday_temp_low_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['low']['fahrenheit']
+    @forecast_next_weekday_conditions = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['conditions']
+    @forecast_next_weekday_icon = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['icon_url']
+    @forecast_next_weekday_wind_mph = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['avewind']['mph']
+    @forecast_next_weekday_wind_kph = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['avewind']['kph']
+    @forecast_next_weekday_humidity = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['avehumidity']
+
+
+
+   p @forecast2 = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['date']['weekday'] 
+   p @forecast2_day = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['date']['day']   
+   p @forecast2_month = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['date']['month']   
+   p @forecast2_year = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['date']['year']   
+   p @forecast2_temp_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['high']['celsius'] 
+   p @forecast2_temp_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['high']['fahrenheit']
+   p @forecast2_temp_low_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['low']['celsius']
+   p @forecast2_temp_low_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['low']['fahrenheit']
+   p @forecast2_conditions = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['conditions']
+   p @forecast2_icon = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['icon_url']
+   p @forecast2_wind_mph = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['avewind']['mph']
+   p @forecast2_wind_kph = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['avewind']['kph']
+   p @forecast2_humidity = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['avehumidity']
+
+
+
+
+
+
+
 
 
     account_sid = ''
