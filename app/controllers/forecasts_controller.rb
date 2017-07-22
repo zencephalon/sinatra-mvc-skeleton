6848bff6cf5ae6ae596889bd6a0a82adcf1e22dc
Iliyan Ganev <ganeviliyan@gmail.com>
2017-07-21 20:01:43 -0700

    Add css and 10 day forecast hard code

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -16,10 +16,7 @@ end
 post '/forecasts' do
   @forecast = Forecast.new(params[:forecast])
   if @forecast.save
-  
-
     redirect "/forecasts/#{@forecast.id}"
-
   else
     @errors = @forecast.errors.full_messages
     erb :'/forecasts/new'
@@ -30,8 +27,119 @@ end
 
 get '/forecasts/:id' do
   @forecast = Forecast.find(params[:id])
- 
+    country = (@forecast.country).split.map(&:capitalize).join('_')
+    city = (@forecast.city).split.map(&:capitalize).join('_')
+    @city = (@forecast.city).split.map(&:capitalize).join(' ')
+    @country = (@forecast.country).split.map(&:capitalize).join(' ')
 
+    weather_json = @forecast.response_weather(country, city)
+    weather10day_jason = @forecast.forecast_10_day(country, city)
+
+    @weather = weather_json['current_observation']['weather']
+    @icon = weather_json['current_observation']['icon_url']
+    @temp = weather_json['current_observation']['temp_c']
+    @temp_f = weather_json['current_observation']['temp_f']
+    @wind = weather_json['current_observation']['wind_kph']
+    @wind_miles = weather_json['current_observation']['wind_mph']
+    @humidity = weather_json['current_observation']['relative_humidity']
+
+
+    @forecast0 = weather10day_jason['forecast']['simpleforecast']['forecastday'][0]['date']['weekday'] 
+    @forecast0_day = weather10day_jason['forecast']['simpleforecast']['forecastday'][0]['date']['day']   
+    @forecast0_month = weather10day_jason['forecast']['simpleforecast']['forecastday'][0]['date']['month']   
+    @forecast0_year = weather10day_jason['forecast']['simpleforecast']['forecastday'][0]['date']['year']   
+    @forecast0_temp_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][0]['high']['celsius'] 
+    @forecast0_temp_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][0]['high']['fahrenheit']
+    @forecast0_temp_low_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][0]['low']['celsius']
+    @forecast0_temp_low_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][0]['low']['fahrenheit']
+    @forecast0_conditions = weather10day_jason['forecast']['simpleforecast']['forecastday'][0]['conditions']
+    @forecast0_icon = weather10day_jason['forecast']['simpleforecast']['forecastday'][0]['icon_url']
+    @forecast0_wind_mph = weather10day_jason['forecast']['simpleforecast']['forecastday'][0]['avewind']['mph']
+    @forecast0_wind_kph = weather10day_jason['forecast']['simpleforecast']['forecastday'][0]['avewind']['kph']
+    @forecast0_humidity = weather10day_jason['forecast']['simpleforecast']['forecastday'][0]['avehumidity']
+
+
+    @forecast1 = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['date']['weekday'] 
+    @forecast1_day = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['date']['day']   
+    @forecast1_month = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['date']['month']   
+    @forecast1_year = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['date']['year']   
+    @forecast1_temp_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['high']['celsius'] 
+    @forecast1_temp_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['high']['fahrenheit']
+    @forecast1_temp_low_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['low']['celsius']
+    @forecast1_temp_low_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['low']['fahrenheit']
+    @forecast1_conditions = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['conditions']
+    @forecast1_icon = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['icon_url']
+    @forecast1_wind_mph = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['avewind']['mph']
+    @forecast1_wind_kph = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['avewind']['kph']
+    @forecast1_humidity = weather10day_jason['forecast']['simpleforecast']['forecastday'][1]['avehumidity']
+
+
+    @forecast2 = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['date']['weekday'] 
+    @forecast2_day = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['date']['day']   
+    @forecast2_month = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['date']['month']   
+    @forecast2_year = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['date']['year']   
+    @forecast2_temp_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['high']['celsius'] 
+    @forecast2_temp_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['high']['fahrenheit']
+    @forecast2_temp_low_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['low']['celsius']
+    @forecast2_temp_low_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['low']['fahrenheit']
+    @forecast2_conditions = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['conditions']
+    @forecast2_icon = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['icon_url']
+    @forecast2_wind_mph = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['avewind']['mph']
+    @forecast2_wind_kph = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['avewind']['kph']
+    @forecast2_humidity = weather10day_jason['forecast']['simpleforecast']['forecastday'][2]['avehumidity']
+
+
+    @forecast3 = weather10day_jason['forecast']['simpleforecast']['forecastday'][3]['date']['weekday'] 
+    @forecast3_day = weather10day_jason['forecast']['simpleforecast']['forecastday'][3]['date']['day']   
+    @forecast3_month = weather10day_jason['forecast']['simpleforecast']['forecastday'][3]['date']['month']   
+    @forecast3_year = weather10day_jason['forecast']['simpleforecast']['forecastday'][3]['date']['year']   
+    @forecast3_temp_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][3]['high']['celsius'] 
+    @forecast3_temp_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][3]['high']['fahrenheit']
+    @forecast3_temp_low_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][3]['low']['celsius']
+    @forecast3_temp_low_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][3]['low']['fahrenheit']
+    @forecast3_conditions = weather10day_jason['forecast']['simpleforecast']['forecastday'][3]['conditions']
+    @forecast3_icon = weather10day_jason['forecast']['simpleforecast']['forecastday'][3]['icon_url']
+    @forecast3_wind_mph = weather10day_jason['forecast']['simpleforecast']['forecastday'][3]['avewind']['mph']
+    @forecast3_wind_kph = weather10day_jason['forecast']['simpleforecast']['forecastday'][3]['avewind']['kph']
+    @forecast3_humidity = weather10day_jason['forecast']['simpleforecast']['forecastday'][3]['avehumidity']
+
+
+    @forecast4 = weather10day_jason['forecast']['simpleforecast']['forecastday'][4]['date']['weekday'] 
+    @forecast4_day = weather10day_jason['forecast']['simpleforecast']['forecastday'][4]['date']['day']   
+    @forecast4_month = weather10day_jason['forecast']['simpleforecast']['forecastday'][4]['date']['month']   
+    @forecast4_year = weather10day_jason['forecast']['simpleforecast']['forecastday'][4]['date']['year']   
+    @forecast4_temp_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][4]['high']['celsius'] 
+    @forecast4_temp_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][4]['high']['fahrenheit']
+    @forecast4_temp_low_celsius = weather10day_jason['forecast']['simpleforecast']['forecastday'][4]['low']['celsius']
+    @forecast4_temp_low_fahrenheit = weather10day_jason['forecast']['simpleforecast']['forecastday'][4]['low']['fahrenheit']
+    @forecast4_conditions = weather10day_jason['forecast']['simpleforecast']['forecastday'][4]['conditions']
+    @forecast4_icon = weather10day_jason['forecast']['simpleforecast']['forecastday'][4]['icon_url']
+    @forecast4_wind_mph = weather10day_jason['forecast']['simpleforecast']['forecastday'][4]['avewind']['mph']
+    @forecast4_wind_kph = weather10day_jason['forecast']['simpleforecast']['forecastday'][4]['avewind']['kph']
+    @forecast4_humidity = weather10day_jason['forecast']['simpleforecast']['forecastday'][4]['avehumidity']
+
+
+    account_sid = ''
+    auth_token = ''
+
+    # set up a client to talk to the Twilio REST API
+  #   client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_AUTH_TOKEN']
+  #   client.account.messages.create({
+  #   :from => ENV['WU_phone'],
+  #   :to => 4157564007,
+  #   :body => "The weather in #{city} is #{@weather}. The temp is #{@temp}, wind: #{@wind}" ,
+  #   # :media_url => 'https://climacons.herokuapp.com/clear.png'
+  # })
+
+
+
+  erb :'forecasts/show'
+end
+
+
+
+get '/forecasts/:id/10day' do
+  @forecast = Forecast.find(params[:id])
     country = (@forecast.country).split.map(&:capitalize).join('_')
     city = (@forecast.city).split.map(&:capitalize).join('_')
     @city = (@forecast.city).split.map(&:capitalize).join(' ')
@@ -214,7 +322,7 @@ get '/forecasts/:id' do
 
 
 
-  erb :'forecasts/show'
+  erb :'forecasts/show10day'
 end
 
 
