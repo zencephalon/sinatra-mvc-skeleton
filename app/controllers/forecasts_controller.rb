0fb11ac937522afe685222abbd74b818f9c051e2
Iliyan Ganev <ganeviliyan@gmail.com>
2017-07-27 17:29:06 -0700

    Add hourly forecast for 34 hour periods

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -36,9 +36,6 @@ get '/forecasts/:id' do
 
     @weather_json = @forecast.response_weather(country, city)
     @weather10day_jason = @forecast.forecast_10_day(country, city)
-
-    p '*' * 40
-    p @weather_json
   
     if @weather_json['response']['error'] == nil && @weather_json['current_observation'] != nil && @weather_json['current_observation']['display_location'] != nil
 
@@ -340,6 +337,35 @@ get '/forecasts/:id/10day' do
 
 
   erb :'forecasts/show10day'
+end
+
+get '/forecasts/:id/hourly' do
+  p '$' * 50
+
+  @forecast = Forecast.find(params[:id])
+    
+  country = (@forecast.country).split.map(&:capitalize).join('_')
+  city = (@forecast.city).split.map(&:capitalize).join('_')
+
+  @weather_json = @forecast.response_weather(country, city)
+  @weather_hourly = @forecast.hourly(country, city)
+
+  # p @weekday = @weather_hourly['hourly_forecast'][0]['FCTTIME']['weekday_name']
+  # p @day = @weather_hourly['hourly_forecast'][0]['FCTTIME']['mday']
+  # p @month = @weather_hourly['hourly_forecast'][0]['FCTTIME']['mon']
+  # p @year = @weather_hourly['hourly_forecast'][0]['FCTTIME']['year']
+  # p @hour = @weather_hourly['hourly_forecast'][0]['FCTTIME']['civil']
+
+  # p @icon = @weather_hourly['hourly_forecast'][0]['icon_url']
+  # p @condition = @weather_hourly['hourly_forecast'][0]['condition']
+  # p @temp_c = @weather_hourly['hourly_forecast'][0]['temp']['metric']
+  # p @temp_f = @weather_hourly['hourly_forecast'][0]['temp']['english']
+  # p @humidity = @weather_hourly['hourly_forecast'][0]['humidity']
+  # p @wind_metric = @weather_hourly['hourly_forecast'][0]['wspd']['metric']
+  # p @wind_english = @weather_hourly['hourly_forecast'][0]['wspd']['english']
+
+  erb :'forecasts/hourly'
+
 end
 
 
--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -4,12 +4,16 @@ class Forecast < ActiveRecord::Base
   validates :country, :city , presence:true
 
 
-  def response_weather(country,city)
+  def response_weather(country, city)
     response = HTTParty.get("http://api.wunderground.com/api/#{ENV['WU_API']}/conditions/q/#{country}/#{city}.json")
   end
 
-  def forecast_10_day(country,city)
+  def forecast_10_day(country, city)
     forecast = HTTParty.get("http://api.wunderground.com/api/#{ENV['WU_API']}/forecast10day/q/#{country}/#{city}.json")
+  end
+
+  def hourly(country, city)
+    hourly = HTTParty.get("http://api.wunderground.com/api/#{ENV['WU_API']}/hourly/q/#{country}/#{city}.json")
   end
 
 end
