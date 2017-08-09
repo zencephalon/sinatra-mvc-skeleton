b3be0041a31a92d9243a7f118854247d52dece47
Iliyan Ganev <ganeviliyan@gmail.com>
2017-08-09 15:44:21 -0700

    Get footer at the bottom of each page to work

--- public/js/application.js
+++ public/js/application.js
@@ -26,3 +26,25 @@ function checkTime(i) {
     return i;
 }
 
+
+
+
+
+// When the user scrolls down 400px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
